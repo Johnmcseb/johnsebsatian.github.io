4b8aedbf3924916cc4bec30a7e14b47725fe7c26
Ethan Li <lietk12@gmail.com>
2016-06-18 12:45:57 -0700

    Removed annoying mouse wheel scroll speed limit. This was a consequence of smooth mousewheel scrolling.

--- static/js/hugo-academic.js
+++ static/js/hugo-academic.js
@@ -51,35 +51,6 @@
   });
 
   /* ---------------------------------------------------------------------------
-   * Smooth scrolling for mouse wheel.
-   * --------------------------------------------------------------------------- */
-
-  function smoothScroll(scrollTime, scrollDistance){
-
-    if (navigator.userAgent.indexOf('Mac') != -1 || navigator.userAgent.indexOf('Firefox') > -1 || jQuery('body').hasClass('is-horizontal')){
-      return;
-    }
-
-    jQuery(window).on("mousewheel DOMMouseScroll", function(event){
-
-      event.preventDefault();
-
-      var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
-      var scrollTop = jQuery(window).scrollTop();
-      var finalScroll = scrollTop - parseInt(delta*scrollDistance);
-
-      TweenMax.to(jQuery(window), scrollTime, {
-        scrollTo : { y: finalScroll, autoKill:true },
-        ease: Expo.easeOut,
-        autoKill: true,
-        overwrite: 5
-      });
-
-    });
-
-  }
-
-  /* ---------------------------------------------------------------------------
    * Hide mobile collapsable menu on clicking a link.
    * --------------------------------------------------------------------------- */
 
@@ -94,9 +65,6 @@
    * --------------------------------------------------------------------------- */
 
   $(window).load(function(){
-
-    // Enable smooth scrolling with mouse wheel
-    smoothScroll(1.3, 220);
 
     // When accessing homepage from another page and `#top` hash is set, show top of page (no hash).
     if (window.location.hash == "#top") {
